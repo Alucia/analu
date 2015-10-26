af62feb2ce704e412aad33b59c1f11112cc75510
Alucia <analu1478@gmail.com>
2015-10-26 15:51:55 -0500

    add

--- js/app.js
+++ js/app.js
@@ -7,26 +7,26 @@ $(document).scroll(function () {
     }
 });
 
-$('.counter').each(function() {
-  var $this = $(this),
-      countTo = $this.attr('data-count');
-  
-  $({ countNum: $this.text()}).animate({
-    countNum: countTo
-  },
-  {
-
-    duration: 8000,
-    easing:'linear',
-    step: function() {
-      $this.text(Math.floor(this.countNum));
+  $('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
     },
-    complete: function() {
-      $this.text(this.countNum);
-      //alert('finished');
-    }
+    {
 
-  });  
-});
+      duration: 8000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+
+    });  
+  });
 
 
